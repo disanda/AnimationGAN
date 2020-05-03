57a261265c1e827f60dd31b5ccb9b4d58d4417cb
disanda <skyanda1@qq.com>
2020-05-03 20:09:05 +0800

    .

--- train_info.py
+++ train_info.py
@@ -80,7 +80,7 @@ transform = torchvision.transforms.Compose([
         torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
 path = '/_yucheng/dataSet/face3D'
-face3d_dataset = torchvision.datasets.ImageFolder(path, transform=transform_face)
+face3d_dataset = torchvision.datasets.ImageFolder(path, transform=transform)
 train_loader = torch.utils.data.DataLoader(face3d_dataset, batch_size=100, shuffle=True,drop_last=True)
 
 
