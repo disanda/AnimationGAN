d2eac8498d1cf53972f4de75ba720ae84415351a
disanda <skyanda1@qq.com>
2020-05-04 17:00:09 +0800

    x

--- train_info.py
+++ train_info.py
@@ -14,7 +14,7 @@ import tqdm
 import random
 #-----------------------prepare of args-------------------
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', dest='experiment_name', default='mnist_cd20_cc10')
+parser.add_argument('--name', dest='experiment_name', default='mnist_cd5_cc5')
 args = parser.parse_args()
 
 
@@ -24,8 +24,8 @@ gpu_mode = True
 SUPERVISED = False
 batch_size = 64
 z_dim_num = 100
-c_d_num = 1
-c_c_num = 2
+c_d_num = 5
+c_c_num = 5
 #input_dim: z =100 ,c_d =10 c_c = 2
 input_size = 64
 img_channel = 1
@@ -95,19 +95,19 @@ train_loader = torch.utils.data.DataLoader(
 
 
 
-# 固定noise和cc，cd每10个变一次
+# 固定noise和cc，每c_d个变一次c_d
 sample_z = torch.zeros((sample_num, z_dim_num))
 temp = torch.zeros((c_d_num, 1))
-for i in range(c_d_num):
-	sample_z[i * 10] = torch.rand(1, z_dim_num)#为0,10,20,30的样本赋值。类似1个类有十个样本
-	for j in range(1, 10):
-		sample_z[i * 10 + j] = sample_z[i * 10]#每10个的noize都相同
+for i in range(sample_num//c_d_num):
+	sample_z[i * c_d_num] = torch.rand(1, z_dim_num)#为连续c_d个的样本赋值。
+	for j in range(c_d_num):
+		sample_z[i * c_d_num + j] = sample_z[i * c_d_num]#相同c_d的noize都相同
 
 for i in range(c_d_num):
 	temp[i, 0] = i #每一个标签
 	temp_d = torch.zeros((sample_num, 1))
-for i in range(c_d_num):
-	temp_d[i * 10: (i + 1) * 10] = temp[i] #每10个人的一样
+for i in range(sample_num//c_d_num):
+	temp_d[i * c_d_num: (i + 1) * c_d_num] = temp[i%c_d_num] #每c_d个的d一样
 sample_d = torch.zeros((sample_num, c_d_num)).scatter_(1, temp_d.type(torch.LongTensor), 1)
 sample_c = torch.zeros((sample_num, c_c_num))
 
@@ -118,7 +118,7 @@ sample_d2 = torch.zeros(sample_num, c_d_num)#[200,c_d]
 temp_c = torch.linspace(-1, 1, c_d_num)		#c_d_num个范围在-1->1的等差数列
 sample_c2 = torch.zeros((sample_num, c_c_num))#[200,c_c]
 
-for i in range(10):		#每20个noise,c_d相同,c_c不同
+for i in range(sample_num//c_d_num):		#每c_d个noise,c_d相同,c_c不同
 	d_label = random.randint(0,c_d_num-1)
 	sample_d2[i*c_d_num:(i+1)*c_d_num, d_label] = 1
 	sample_c2[i*c_d_num:(i+1)*c_d_num,i%c_c_num] = temp_c
