471d18f466c519528f1b01154c6a95179740542b
disanda <skyanda1@qq.com>
2020-05-04 13:28:58 +0800

    x

--- train_info.py
+++ train_info.py
@@ -14,7 +14,7 @@ import tqdm
 import random
 #-----------------------prepare of args-------------------
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', dest='experiment_name', default='mnist_cd5_cc5')
+parser.add_argument('--name', dest='experiment_name', default='mnist_cd1_cc2')
 args = parser.parse_args()
 
 
@@ -24,8 +24,8 @@ gpu_mode = True
 SUPERVISED = False
 batch_size = 64
 z_dim_num = 100
-c_d_num = 5
-c_c_num = 5
+c_d_num = 1
+c_c_num = 2
 #input_dim: z =100 ,c_d =10 c_c = 2
 input_size = 64
 img_channel = 1
