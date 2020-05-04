a49159d94e5029b4acdcfd277f2360740174b87e
disanda <skyanda1@qq.com>
2020-05-04 22:44:10 +0800

    x

--- loss_norm_gp.py
+++ loss_norm_gp.py
@@ -12,16 +12,6 @@ def get_losses_fn(mode):
             r_loss = torch.nn.functional.binary_cross_entropy_with_logits(r_logit, torch.ones_like(r_logit))
             f_loss = torch.nn.functional.binary_cross_entropy_with_logits(f_logit, torch.zeros_like(f_logit))
             return r_loss, f_loss
-
-        def g_loss_fn(f_logit):
-            f_loss = torch.nn.functional.binary_cross_entropy_with_logits(f_logit, torch.ones_like(f_logit))
-            return f_loss
-
-    if model == 'gan_v2'
-        def d_loss_fn(r_logit, f_logit):
-            r_loss = torch.nn.functional.binary_cross_entropy_with_logits(r_logit, torch.ones_like(r_logit))
-            f_loss = torch.nn.functional.binary_cross_entropy_with_logits(f_logit, torch.zeros_like(f_logit))
-            return r_loss, f_loss
         def g_loss_fn(f_logit):
             f_loss = torch.nn.functional.binary_cross_entropy_with_logits(f_logit, torch.ones_like(f_logit))
             return f_loss
@@ -31,7 +21,6 @@ def get_losses_fn(mode):
             r_loss = torch.nn.functional.mse_loss(r_logit, torch.ones_like(r_logit))
             f_loss = torch.nn.functional.mse_loss(f_logit, torch.zeros_like(f_logit))
             return r_loss, f_loss
-
         def g_loss_fn(f_logit):
             f_loss = torch.nn.functional.mse_loss(f_logit, torch.ones_like(f_logit))
             return f_loss
@@ -41,7 +30,6 @@ def get_losses_fn(mode):
             r_loss = -r_logit.mean()
             f_loss = f_logit.mean()
             return r_loss, f_loss
-
         def g_loss_fn(f_logit):
             f_loss = -f_logit.mean()
             return f_loss
@@ -51,7 +39,6 @@ def get_losses_fn(mode):
             r_loss = torch.max(1 - r_logit, torch.zeros_like(r_logit)).mean()
             f_loss = torch.max(1 + f_logit, torch.zeros_like(f_logit)).mean()
             return r_loss, f_loss
-
         def g_loss_fn(f_logit):
             f_loss = torch.max(1 - f_logit, torch.zeros_like(f_logit)).mean()
             return f_loss
@@ -61,7 +48,6 @@ def get_losses_fn(mode):
             r_loss = torch.max(1 - r_logit, torch.zeros_like(r_logit)).mean()
             f_loss = torch.max(1 + f_logit, torch.zeros_like(f_logit)).mean()
             return r_loss, f_loss
-
         def g_loss_fn(f_logit):
             f_loss = - f_logit.mean()
             return f_loss
