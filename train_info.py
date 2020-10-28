47b5bf6fd4b6a2c02224c4d8d8348502eb0cb086
disanda <skyanda1@qq.com>
2020-10-28 16:48:12 +0800

    x

--- train_info.py
+++ train_info.py
@@ -16,7 +16,7 @@ import loss_norm_gp
 import functools
 #-----------------------prepare of args-------------------
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', dest='experiment_name', default='movingmnist_1_wmw+_cd10_cc12')
+parser.add_argument('--name', dest='experiment_name', default='movingmnist_2_wmw+_cd10_cc12')
 args = parser.parse_args()
 
 
