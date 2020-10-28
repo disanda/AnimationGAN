b2dd07470bb9fe553f80e2fdfd2260acfabb85cb
disanda <skyanda1@qq.com>
2020-10-28 17:33:53 +0800

    x

--- train_info_cd20_cc10.py
+++ train_info_cd20_cc10.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import os
 import numpy as np
 import itertools
-import model.model as model
+import model.model_v2 as model
 import argparse
 from PIL import Image
 import time
