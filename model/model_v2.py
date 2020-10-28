1116955e767d72af2e3ca5a157b3765705d0fc7b
disanda <skyanda1@qq.com>
2020-10-28 16:46:29 +0800

    x

--- model/model_v2.py
+++ model/model_v2.py
@@ -66,7 +66,7 @@ class discriminator_mwm(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Linear(1024, self.output_dim + self.len_continuous_code + self.len_discrete_code),
             #nn.BatchNorm1d(self.output_dim + self.len_continuous_code + self.len_discrete_code),
-            nn.LeakyReLU(0.2),
+            #nn.LeakyReLU(0.2),
             # nn.Sigmoid(),
         )
         loss_norm_gp.initialize_weights(self)
