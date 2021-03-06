c3380716969371b993a7609afd065d019bc3f885
disanda <skyanda1@qq.com>
2021-03-06 18:39:27 +0800

    x

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CGAN-ACGAN-INFOGAN
+# AnimationGAN
+
+  >技术线：CGAN-ACGAN-INFOGAN
+
+这是论文[SSL_GAN](https://www.researchgate.net/publication/342909538_Self-Supervised_Animation_Synthesis_Through_Adversarial_Training)的源码
+
+## ablation study
+
+在文件夹ccNcdN中, 比如：
+	>train_info_cd20_cc10.py
+
+## 数据集
+
+- 2D shapes
+
+- 3D face
+
+- celebaA
+
+- Moving-Mnist(乱序的)
+
+- Human Action(来自MoCoGAN)
+
+
+good luck!
--- train_info.py
+++ train_info.py
@@ -28,6 +28,7 @@ batch_size = 100
 z_dim_num = 104
 c_d_num = 10
 c_c_num = 12
+sample_num=400
 #input_dim: z =100 ,c_d =10 c_c = 2
 input_size = 64
 img_channel = 1 
@@ -131,20 +132,38 @@ train_loader = torch.utils.data.DataLoader(
 
 
 #----------------------固定测试变量--------------------
-sample_num =120 # 10*12, c_d10个 c_c 12个
 
+sample_z = torch.zeros((sample_num, z_dim_num))
+temp = torch.zeros((c_d_num, 1))
+for i in range(sample_num//c_d_num):
+	sample_z[i * c_d_num] = torch.rand(1, z_dim_num)#为连续c_d个的样本赋值。
+	for j in range(c_d_num):
+		sample_z[i * c_d_num + j] = sample_z[i * c_d_num]#相同c_d的noize都相同
+
+for i in range(c_d_num):
+	temp[i, 0] = i #每一个标签
+temp_d = torch.zeros((sample_num, 1))
+for i in range(sample_num//c_d_num):
+	temp_d[i * c_d_num: (i + 1) * c_d_num] = temp[i%c_d_num] #每c_d个的d一样
+sample_d = torch.zeros((sample_num, c_d_num)).scatter_(1, temp_d.type(torch.LongTensor), 1)
+sample_c = torch.zeros((sample_num, c_c_num))
+
+# 观察c_c变量，固定其他变量(置0)
+sample_z2 = torch.rand((1, z_dim_num)).expand(sample_num, z_dim_num) #每个样本的noize相同
+sample_d2 = torch.zeros(sample_num, c_d_num)#[200,c_d]
+
+temp_c = torch.linspace(-1, 1, c_d_num)		#c_d_num个范围在-1->1的等差数列
+sample_c2 = torch.zeros((sample_num, c_c_num))#[200,c_c]
+
+for i in range(sample_num//c_d_num):		#每c_d个noise,c_d相同,c_c不同
+	#d_label = random.randint(0,c_d_num-1)
+	d_label = i%c_d_num
+	sample_d2[i*c_d_num:(i+1)*c_d_num, d_label] = 1
+	sample_c2[i*c_d_num:(i+1)*c_d_num,i%c_c_num] = temp_c
+
+sample_num =120 # 10*12, c_d10个 c_c 12个
 sample_z = torch.randn(sample_num, z_dim_num)
 
-sample_d = torch.zeros(sample_num, c_d_num) #每12个，c_d换一个维度
-for i in range(10):
-	for j in range(12): 
-		sample_d[j+i*12,i]=1
-
-sample_c = torch.zeros(sample_num, c_c_num) #每12个内，c_c值变换, 之后c_c换一个维度
-temp = torch.linspace(-10,10,steps=12)
-for i in range(10):
-	for j in range(12): 
-		sample_c[j+i*12,i]=temp[j]
 
 #gpu
 if gpu_mode == True:
